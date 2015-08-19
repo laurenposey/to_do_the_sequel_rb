f6f408643fa13bb17f299a2c3f1abd4defc34ca6
Kelly Becker <kelly@epicodus.com>
2015-08-19 12:15:31 -0700

    add initial lib and spec files for task

---
+++ spec/task_spec.rb
@@ -0,0 +1,29 @@
+require('rspec')
+require('task')
+
+describe(Task) do
+  before() do
+    Task.clear()
+  end
+  
+  describe("#description") do
+    it("lets you give it a description") do
+      test_task = Task.new("scrub the zebra")
+      expect(test_task.description()).to(eq("scrub the zebra"))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved tasks") do
+      Task.new("wash the lion").save()
+      Task.clear()
+      expect(Task.all()).to(eq([]))
+    end
+  end
+end
